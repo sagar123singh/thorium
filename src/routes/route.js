99ec90a00f277b9a620c71c4b7521c09092b9278
sagar123singh <s9813558351@gmail.com>
2022-02-23 08:58:54 +0530

    my first api

--- src/routes/route.js
+++ src/routes/route.js
@@ -8,6 +8,37 @@ router.get('/students/:name', function(req, res) {
 })
 
 
+router.get('/movies', function(req,res){
+    const movies =["tarzan","wanted","Gadar","Sholay","pushpa"]
+    res.send(movies)
+})
 
+
+router.get('movies/:index', function(req,res){
+ const index= req.params.index
+    const movies=["tarzan","wanted","Gadar","Sholay","pushpa"]
+    if(index>movies.length){
+        res.send("use a valid index")
+    }else{
+        res.send(movies[index-1])
+    }
+})
+
+
+router.get('/moviez', function (req,res){
+    const moviez=[ {id: 1,name: 'the shininig'},{id: 2,name: 'incendies'},{id: 3,name: 'idiots'},{id: 4,name: 'rang de basanti'}]
+res.send(moviez)
+})
+
+
+router.get('/moviez/:moviezId', function (req,res){
+    const moviezId=req.params.filmId
+    const moviez=[ {id: 1,name: 'the shininig'},{id: 2,name: 'incendies'},{id: 3,name: 'idiots'},{id: 4,name: 'rang de basanti'}]
+    if(moviezId>moviez.length){
+        res.send("no movie exists with this Id")
+    }else{
+        res.send(moviez[moviezId-1])
+    }
+})
 
 module.exports = router;
