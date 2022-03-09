88edd06db280773e84c380ca3eb1e447b799e211
sagar123singh <s9813558351@gmail.com>
2022-03-10 00:17:17 +0530

    authorise

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/skDB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/sagarsolanki-DB?authSource=admin&replicaSet=atlas-9zusex-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,12 +1,46 @@
+const jwt = require("jsonwebtoken")
+const userModel = require("../models/userModel");
+const router = require("../routes/route");
 const authenticate = function(req, req, next) {
-    //check the token in request header
-    //validate this token
 
+
+    let token = req.headers["x-auth-token"];
+    if (!token) 
+  
+    if (!token) return res.send({ status: false, msg: "token must be present" });
+  
+    console.log(token);
+    
+    let decodedToken = jwt.verify(token, "functionup-thorium");
+    if (!decodedToken)
+      return res.send({ status: false, msg: "token is invalid" });
+  
+     
     next()
 }
-
 
 const authorise = function(req, res, next) {
+    let token = req.headers["x-auth-token"]
+    if(!token) return res.send({status: false, msg: "token must be present in the request header"})
+    let decodedToken = jwt.verify(token, 'functionup-thorium')
+
+    if(!decodedToken) return res.send({status: false, msg:"token is not valid"})
+    
+    //userId for which the request is made. In this case message to be posted.
+    let userToBeModified = req.params.userId
+    //userId for the logged-in user
+    let userLoggedIn = decodedToken.userId
+
+    //userId comparision to check if the logged-in user is requesting for their own data
+    if(userToBeModified != userLoggedIn) return res.send({status: false, msg: 'User logged is not allowed to modify the requested users data'})
+
     // comapre the logged in user's id and the id in request
+
+
+
+
     next()
 }
+
+module.exports.authenticate=authenticate
+module.exports.authorise=authorise
